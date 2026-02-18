a56719699892856359b3786a2856cf98eeda0383
royabradley <99550591+royabradley@users.noreply.github.com>
2026-02-18 16:19:06 +0000

    Deploying to gh-pages from @ royabradley/royabradley.github.io@840d883022950e3378588c2b4b38b8f113310b18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-quot-the-gospel-of-the-kingdom-quot",
         
           title: "&quot;The Gospel of the Kingdom&quot;",
