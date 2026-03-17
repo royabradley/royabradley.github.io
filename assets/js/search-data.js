fd8c52d7f34201af0f8dedc8cf974dcc45526f2b
royabradley <99550591+royabradley@users.noreply.github.com>
2026-03-17 12:10:01 +0000

    Deploying to gh-pages from @ royabradley/royabradley.github.io@292d6f69e28272f4aebc887f90acc8eb1e45a9f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "post-the-impeded-stream",
+  },{id: "post-the-impeded-stream",
         
           title: "The Impeded Stream",
         
